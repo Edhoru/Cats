0ed55d684133cf2af4584be17bea9c417b15cb8d
Alberto <edhoru@hotmail.com>
2024-06-07 07:18:43 -0600

    widget wip

---
+++ CatsWidget/CatsWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CatsWidgetBundle.swift
+//  CatsWidget
+//
+//  Created by Alberto on 06/06/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CatsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CatsWidget()
+    }
+}
